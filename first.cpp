2e218d9ba07ddca0e4993760ae751942baba9752
Abdullah Al Noman <Abdullah.Noman@dizishore.com>
2025-06-08 14:20:35 +0600

    primitive data

---
+++ first.cpp
@@ -0,0 +1,21 @@
+// pre processor directives
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age = 29;
+    short smallNum = 100;
+    long population = 100000L;
+    long long bigNum = 1234567799343LL;
+    cout << bigNum << endl;
+    float prince = 19.99f;
+    double pi = 3.1415922244;
+    cout << prince << endl
+         << pi << endl;
+    char grade = 'A';
+    bool isActive = true;
+    
+    return 0;
+}
